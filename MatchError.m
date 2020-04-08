ae2ea223e3c508e9229471dd1bc73836a29f01ee
hehesjtu <jiangherenren@163.com>
2020-04-08 21:38:27 +0800

    Delete MatchError.m

--- MatchError.m
+++
@@ -1,7 +0,0 @@
-function weight = MatchError(img1,img2)
-          img1 = double(img1);
-          img2 = double(img2);
-          dis = sum(abs(img1(:)-img2(:)));
-          weight = double(1/dis);
-end
-
